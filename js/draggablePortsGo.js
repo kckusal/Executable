d84116538ebe99205a2ca0015a90106a429207d9
Kusal KC <kckusal99@gmail.com>
2018-06-29 11:12:42 +0300

    Update draggablePortsGo.js

--- js/draggablePortsGo.js
+++ js/draggablePortsGo.js
@@ -8,7 +8,7 @@ function init() {
     "linkDataArray": []
   };
 
-  if (localStorage.getItem("AppModel") === null) {
+  if (localStorage.getItem("AppModel") == null) {
     localStorage.setItem("AppModel", JSON.stringify(setInitComponent));
   }
   
